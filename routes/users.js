44760f77335d934cf5a6446ccaa30fbf8288c5bb
Lov??sa S??l <kik7@hi.is>
2026-03-16 12:42:22 +0000

    fix activate user bug

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,7 @@ router.post('/:id(\\d+)/activate', loadUser('id'), authorize(ABILITIES.USER_ACTI
   try {
     const userToActivate = req.resource.user;
 
-    usersRepo.updateUser(userToActivate.id, { is_active: false });
+    usersRepo.updateUser(userToActivate.id, { is_active: true });
 
     safeAuditLog(req, {
       event_type: 'admin_activate_user',
